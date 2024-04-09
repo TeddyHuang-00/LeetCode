9a1cbcec191a4218ec9b3f5598cc48c4824fa473
TeddyHuang-00 <huang_nan_2019@pku.edu.cn>
2024-04-09 19:59:35 -0400

    fix: :bug: Fix bug by moving to DP rather than greedy

--- src/lc1547.rs
+++ src/lc1547.rs
@@ -4,23 +4,21 @@ impl Solution {
     pub fn min_cost(n: i32, cuts: Vec<i32>) -> i32 {
         let mut cuts = cuts;
         cuts.sort();
-        let mut segments = vec![0];
-        segments.extend(cuts.to_owned());
-        segments.push(n);
-        let mut segments = segments.windows(2).map(|w| w[1] - w[0]).collect::<Vec<_>>();
-        let mut cost = 0;
-        while cuts.len() > 0 {
-            let (w, idx) = segments
-                .windows(2)
-                .zip(0..)
-                .min_by(|(a, _), (c, _)| (a[0] + a[1]).cmp(&(c[0] + c[1])))
-                .unwrap();
-            cost += w[0] + w[1];
-            segments[idx] += segments[idx + 1];
-            segments.remove(idx + 1);
-            cuts.remove(idx);
+        cuts.insert(0, 0);
+        cuts.push(n);
+        let n = cuts.len();
+        let mut dp = vec![vec![std::i32::MAX; n]; n];
+        for i in 0..cuts.len() - 1 {
+            dp[i][i + 1] = 0;
         }
-        cost
+        for num_segs in 2..n {
+            for (i, j) in (0..n).zip(num_segs..n) {
+                let step_cost = cuts[j] - cuts[i];
+                dp[i][j] = dp[i][j]
+                    .min((i + 1..j).map(|k| dp[i][k] + dp[k][j]).min().unwrap_or(0) + step_cost);
+            }
+        }
+        dp[0][cuts.len() - 1]
     }
 }
 
@@ -36,5 +34,16 @@ mod tests {
     #[test]
     fn testcase_2() {
         assert_eq!(Solution::min_cost(9, vec![5, 6, 1, 4, 2]), 22)
+    }
+
+    #[test]
+    fn testcase_3() {
+        assert_eq!(
+            Solution::min_cost(
+                36,
+                vec![13, 17, 15, 18, 3, 22, 27, 6, 35, 7, 11, 28, 26, 20, 4, 5, 21, 10, 8]
+            ),
+            150
+        )
     }
 }
