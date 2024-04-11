96cbaaec042a22d7b248725853d50475809347b6
TeddyHuang-00 <huang_nan_2019@pku.edu.cn>
2024-04-11 14:17:05 -0400

    feat: :sparkles: Add solution for LC1879 - Minimum XOR Sum of Two Arrays

---
+++ src/lc1879.rs
@@ -0,0 +1,68 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn minimum_xor_sum(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
+        let mask = (0..nums1.len()).collect::<Vec<_>>();
+        let mut cache = std::collections::HashMap::new();
+        Solution::dp(&nums1, &nums2, &mask, &mut cache)
+    }
+
+    fn dp(
+        nums1: &Vec<i32>,
+        nums2: &Vec<i32>,
+        mask: &Vec<usize>,
+        cache: &mut std::collections::HashMap<usize, i32>,
+    ) -> i32 {
+        if mask.is_empty() {
+            return 0;
+        }
+        let h = mask.iter().fold(0, |acc, idx| acc + (1 << idx));
+        if let Some(&v) = cache.get(&h) {
+            return v;
+        }
+        let mut res = std::i32::MAX;
+        for (idx, m) in mask.iter().enumerate() {
+            let mut new_mask = mask.clone();
+            new_mask.remove(idx);
+            res = res.min(
+                (nums1[*m] ^ nums2[new_mask.len()]) + Solution::dp(nums1, nums2, &new_mask, cache),
+            );
+        }
+        cache.insert(h, res);
+        res
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn testcase_1() {
+        assert_eq!(Solution::minimum_xor_sum(vec![1, 2], vec![2, 3]), 2)
+    }
+
+    #[test]
+    fn testcase_2() {
+        assert_eq!(Solution::minimum_xor_sum(vec![1, 0, 3], vec![5, 3, 4]), 8)
+    }
+
+    #[test]
+    fn testcase_3() {
+        assert_eq!(
+            Solution::minimum_xor_sum(vec![72, 97, 8, 32, 15], vec![63, 97, 57, 60, 83]),
+            152
+        )
+    }
+
+    #[test]
+    fn testcase_4() {
+        assert_eq!(
+            Solution::minimum_xor_sum(
+                vec![100, 26, 12, 62, 3, 49, 55, 77, 97],
+                vec![98, 0, 89, 57, 34, 92, 29, 75, 13]
+            ),
+            200
+        )
+    }
+}
--- src/problems.rs
+++ src/problems.rs
@@ -1,4 +1,5 @@
 pub mod lc1547;
+pub mod lc1879;
 pub mod lc1944;
 pub mod lc2209;
 pub mod lc2940;
